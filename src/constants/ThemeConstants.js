a21ae882668e9f6bc3020b0d19c465d28cf1c7df
David <d.li.2247@gmail.com>
2019-08-29 21:52:14 -0400

    Add basic search box

---
+++ src/constants/ThemeConstants.js
@@ -0,0 +1,14 @@
+const Theme = Object.freeze({
+	MARGIN: {
+		SMALL: '8px',
+		MED: '12px',
+		LARGE: '16px'
+	},
+	INPUT: {
+		LARGE: '28px',
+		MED: '20px',
+		SMALL: '14px'
+	}
+});
+
+export { Theme };
